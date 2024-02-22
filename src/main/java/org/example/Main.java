f716ff149fb0976d2be7bdd7aab4cd5c9b2a1100
chenriqueweb <carlos.henrique.mobile@gmail.com>
2024-02-22 19:17:30 -0300

    Start

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,58 @@
 package org.example;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.ssl.TrustStrategy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ApiController apiController = new ApiController();
-        apiController.chamarAPI("10");
+        apiController.chamarAPI("30");
     }
+
+//    @Bean
+//    public RestTemplate restTemplate() throws Exception {
+//        SSLContext sslContext = new SSLContextBuilder()
+//                .loadKeyMaterial(keyStore, keyStorePassword.toCharArray(), keyStorePassword.toCharArray())
+//                .loadTrustMaterial(trustStore, trustStorePassword.toCharArray(), (cert, authType) -> sslTrustStrategy)
+//                .build();
+//        HostnameVerifier hostnameVerifier = sslTrustStrategy ? new NoopHostnameVerifier() :
+//                SSLConnectionSocketFactory.getDefaultHostnameVerifier();
+//        SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext, hostnameVerifier);
+//        HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslSocketFactory).build();
+//        HttpComponentsClientHttpRequestFactory httpComponentsHttpClientFactory =
+//                new HttpComponentsClientHttpRequestFactory(httpClient);
+//        RestTemplate restTemplate = new RestTemplate(httpComponentsHttpClientFactory);
+//        return restTemplate;
+//    }
 }
